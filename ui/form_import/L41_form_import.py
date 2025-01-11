20eae9ba638917b6d978deaef0d220af7973a9c4
e_vladimir <79600069487@ya.ru>
2025-01-11 14:58:26 +0300

    Форма Импорт данных: * Рефакторинг меню

--- ui/form_import/L41_form_import.py
+++ ui/form_import/L41_form_import.py
@@ -17,20 +17,25 @@ class C41_FormImport(C20_PySideForm, Ui_frm_import):
 
 	def InitMenuOperation(self):
 		""" Инициализация меню Импорта операций """
-		icon_download                                           = QIcon("./L0/icons/download.svg")
-		icon_edit                                               = QIcon("./L0/icons/edit.svg")
-		icon_grid_3_3                                           = QIcon("./L0/icons/grid_3_3.svg")
-		icon_list                                               = QIcon("./L0/icons/list.svg")
-		icon_open                                               = QIcon("./L0/icons/open.svg")
-		icon_reload                                             = QIcon("./L0/icons/reload.svg")
+		icon_download = QIcon("./L0/icons/download.svg")
+		icon_edit     = QIcon("./L0/icons/edit.svg")
+		icon_grid_3_3 = QIcon("./L0/icons/grid_3_3.svg")
+		icon_list     = QIcon("./L0/icons/list.svg")
+		icon_open     = QIcon("./L0/icons/open.svg")
+		icon_reload   = QIcon("./L0/icons/reload.svg")
 
-		self.menu_operations                                    = QMenu("Финансовые операции")
+		self.action_operations_operations_open_file   = QAction(icon_open,     "Открыть файл")
+		self.action_operations_operations_switch_data = QAction(icon_reload,   "Сменить набор данных")
+		self.action_operations_operations_exec_import = QAction(icon_download, "Импортировать данные")
+		self.action_operations_header_set_field       = QAction(icon_edit,     "Указать тип данных")
 
-		self.submenu_operations_operations                      = self.menu_operations.addMenu(icon_list, "Финансовые операции")
-		self.action_operations_operations_open_file   : QAction = self.submenu_operations_operations.addAction(icon_open,     "Открыть файл")
-		self.action_operations_operations_switch_data : QAction = self.submenu_operations_operations.addAction(icon_reload,   "Сменить набор данных")
+		self.menu_operations                          = QMenu("Финансовые операции")
+
+		self.submenu_operations_operations            = self.menu_operations.addMenu(icon_list, "Финансовые операции")
+		self.submenu_operations_operations.addAction(self.action_operations_operations_open_file)
+		self.submenu_operations_operations.addAction(self.action_operations_operations_switch_data)
 		self.submenu_operations_operations.addSeparator()
-		self.action_operations_operations_exec_import : QAction = self.submenu_operations_operations.addAction(icon_download, "Импортировать данные")
+		self.submenu_operations_operations.addAction(self.action_operations_operations_exec_import)
 
-		self.submenu_operations_header                          = self.menu_operations.addMenu(icon_grid_3_3, "Элемент заголовка файла")
-		self.action_operations_header_set_field       : QAction = self.submenu_operations_header.addAction(icon_edit, "Указать тип данных")
+		self.submenu_operations_header                = self.menu_operations.addMenu(icon_grid_3_3, "Элемент заголовка файла")
+		self.submenu_operations_header.addAction(self.action_operations_header_set_field)
