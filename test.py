546f57bed35f3551a364aa5c11b645fb8650888c
e_vladimir <79600069487@ya.ru>
2025-04-06 17:14:41 +0300

    * Фиксация изменений в библиотеке

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-val = "0x00"
-
-print("isalnum     ", val.isalnum())
-print("isalpha     ", val.isalpha())
-print("isascii     ", val.isascii())
-print("isdecimal   ", val.isdecimal())
-print("isdigit     ", val.isdigit())
-print("isidentifier", val.isidentifier())
-print("islower     ", val.islower())
-print("isnumeric   ", val.isnumeric())
-print("isprintable ", val.isprintable())
-print("isspace     ", val.isspace())
-print("istitle     ", val.istitle())
-print("isupper     ", val.isupper())
+print([i for i in range(ord('а'), ord('я') + 1)])
